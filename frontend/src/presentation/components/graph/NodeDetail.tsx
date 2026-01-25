9667641a98f4a36b7cd2701673b56528e6b8aa47
Hayochan-man <taropado3.14@gmail.com>
2026-01-25 10:52:27 +0900

    next.jsをexportにし、dockerもこれに修正

--- frontend/src/presentation/components/graph/NodeDetail.tsx
+++
@@ -1,6 +0,0 @@
-'use client';
-
-// Redirect NodeDetail to use KnowledgeDetail for consistency in the graph view
-import KnowledgeDetail from '@/presentation/components/search/KnowledgeDetail';
-
-export default KnowledgeDetail;
